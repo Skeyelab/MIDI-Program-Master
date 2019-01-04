9d2336c73d0f6d2106c12a43a5c507a9e6bf083a
Frankie Claessens <claessensfrankie@gmail.com>
2019-01-04 01:53:46 +0100

    incoming CCs are ok

--- src/app/components/settings/settings.component.ts
+++ src/app/components/settings/settings.component.ts
@@ -1,4 +1,4 @@
-import {Component, Input, OnChanges, OnInit, SimpleChanges} from '@angular/core';
+import {Component, Input, OnChanges, SimpleChanges} from '@angular/core';
 import {ChannelSetting} from "../../../models/channel-setting";
 
 @Component({
@@ -11,10 +11,11 @@ export class SettingsComponent implements OnChanges {
   amountOfChannel = 16;
   @Input() io: any;
 
-  constructor() {}
+  constructor() {
+  }
 
   ngOnChanges(changes: SimpleChanges): void {
-    if(changes.hasOwnProperty('io')) {
+    if (changes.hasOwnProperty('io')) {
       this.generateChannels();
     }
   }
@@ -23,16 +24,48 @@ export class SettingsComponent implements OnChanges {
     this.settings = [];
     Array.from(Array(this.amountOfChannel).keys()).map(index => {
       this.settings.push({
-        channel: index + 1
+        channel: index + 1,
+        ccs: []
       });
     });
-    this.enableListeners();
+    this.enableInputListeners();
   }
 
-  enableListeners() {
-    if(this.io) {
+  enableInputListeners() {
+    if (this.io) {
       this.io.inputs.forEach(input => {
+        input.addListener('controlchange', 'all', function (e) {
+          switch (e.controller.name) {
+            case 'bankselectcoarse':
+              console.log(`Updating MSB for ch: ${e.channel} to ${e.value}`);
+              this.settings.find(setting => setting.channel === e.channel).bankSelectCoarse = e.value;
+              break;
+            case 'bankselectfine':
+              console.log(`Updating LSB for ch: ${e.channel} to ${e.value}`);
+              this.settings.find(setting => setting.channel === e.channel).bankSelectFine = e.value;
+              break;
+            default:
+              console.log('Received controlchange message.', e);
+              const setting = this.settings.find(setting => setting.channel === e.channel);
+              const cc = setting.ccs.find(cc => cc.number === e.controller.number);
+              if (cc) {
+                cc.value = e.value
+              } else {
+                setting.ccs.push({
+                  number: e.controller.number,
+                  value: e.value
+                })
+              }
+          }
+        }.bind(this));
 
+        input.addListener('programchange', 'all', function (e) {
+          console.log(`Updating PC for ch: ${e.channel} to ${e.value}`);
+          this.settings.filter(setting => setting.channel === e.channel).forEach(setting => {
+            setting.programChange = e.value;
+            setting.ccs = [];
+          });
+        }.bind(this));
       });
     }
   }
--- src/models/channel-setting.ts
+++ src/models/channel-setting.ts
@@ -2,8 +2,8 @@ import {CcSetting} from "./cc-setting";
 
 export interface ChannelSetting {
   channel: number;
-  bankSelectorCourse?: number;
-  bankSelectorFine?: number;
+  bankSelectCoarse?: number;
+  bankSelectFine?: number;
   programChange?: number;
   ccs?: Array<CcSetting>;
 }
